4207ca5e16f44346ec5c06c66abe0b1efc1d71b5
Simon Da Vall <simon.davall@gmail.com>
2026-07-13 00:00:26 +0100

    sot-4: Established the MudBlazor application layout

---
+++ Soteria2/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Soteria2.Components.Layout;
+
+public partial class MainLayout
+{
+    private bool _drawerOpen = true;
+
+    private void ToggleDrawer()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+}
