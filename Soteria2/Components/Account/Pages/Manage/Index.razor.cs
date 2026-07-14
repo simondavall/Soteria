8e50da31a180861d95b703c0cc5963f7c6137fbf
Simon Da Vall <simon.davall@gmail.com>
2026-07-14 09:36:55 +0100

    sot-10: Migrated Manage Index, ManageLayout and ManageNavMenu pages

---
+++ Soteria2/Components/Account/Pages/Manage/Index.razor.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Soteria2.Data;
+
+namespace Soteria2.Components.Account.Pages.Manage;
+
+public partial class Index
+{
+    private ApplicationUser _user = default!;
+    private string? _username;
+    private string? _phoneNumber;
+
+    [Inject] private UserManager<ApplicationUser> UserManager { get; set; } = default!;
+    [Inject] private SignInManager<ApplicationUser> SignInManager { get; set; } = default!;
+    [Inject] private IdentityRedirectManager RedirectManager { get; set; } = default!;
+
+    [CascadingParameter] private HttpContext HttpContext { get; set; } = default!;
+    [SupplyParameterFromForm] private InputModel Input { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        var user = await UserManager.GetUserAsync(HttpContext.User);
+        if (user is null)
+        {
+            RedirectManager.RedirectToInvalidUser(UserManager, HttpContext);
+            return;
+        }
+
+        _user = user;
+        _username = await UserManager.GetUserNameAsync(_user);
+        _phoneNumber = await UserManager.GetPhoneNumberAsync(_user);
+
+        if (HttpMethods.IsGet(HttpContext.Request.Method))
+        {
+            Input.PhoneNumber = _phoneNumber;
+        }
+    }
+
+    private async Task OnValidSubmitAsync()
+    {
+        if (Input.PhoneNumber != _phoneNumber)
+        {
+            var setPhoneResult = await UserManager.SetPhoneNumberAsync(_user, Input.PhoneNumber);
+            if (!setPhoneResult.Succeeded)
+            {
+                RedirectManager.RedirectToCurrentPageWithStatus("Error: Failed to set phone number.", HttpContext);
+                return;
+            }
+        }
+
+        await SignInManager.RefreshSignInAsync(_user);
+
+        RedirectManager.RedirectToCurrentPageWithStatus("Your profile has been updated.", HttpContext);
+    }
+
+    private sealed class InputModel
+    {
+        private string? _phoneNumber;
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string? PhoneNumber
+        {
+            get => _phoneNumber;
+
+            set => _phoneNumber = string.IsNullOrWhiteSpace(value)
+                ? null
+                : value;
+        }
+    }
+}
