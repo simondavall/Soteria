9074611fa57ef574c3bce87c5e184c294686bbf3
Simon Da Vall <simon.davall@gmail.com>
2026-07-14 11:27:00 +0100

    sot-12: Migrated GenerateRecoveryCodes, ResetAuthenticator and TwoFactorAuthentication pages

---
+++ Soteria2/Components/Account/Pages/Manage/GenerateRecoveryCodes.razor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Soteria2.Data;
+
+namespace Soteria2.Components.Account.Pages.Manage;
+
+public partial class GenerateRecoveryCodes
+{
+    private string? _message;
+    private ApplicationUser? _user;
+    private IEnumerable<string>? _recoveryCodes;
+
+    [Inject] private UserManager<ApplicationUser> UserManager { get; set; } = default!;
+    [Inject] private IdentityRedirectManager RedirectManager { get; set; } = default!;
+    [Inject] private ILogger<GenerateRecoveryCodes> Logger { get; set; } = default!;
+
+    [CascadingParameter] private HttpContext HttpContext { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _user = await UserManager.GetUserAsync(HttpContext.User);
+        if (_user is null)
+        {
+            RedirectManager.RedirectToInvalidUser(UserManager, HttpContext);
+            return;
+        }
+
+        var isTwoFactorEnabled = await UserManager.GetTwoFactorEnabledAsync(_user);
+
+        if (!isTwoFactorEnabled)
+        {
+            throw new InvalidOperationException(
+                "Cannot generate recovery codes because two-factor authentication is not enabled.");
+        }
+    }
+
+    private async Task OnSubmitAsync()
+    {
+        if (_user is null)
+        {
+            RedirectManager.RedirectToInvalidUser(UserManager, HttpContext);
+            return;
+        }
+
+        var userId = await UserManager.GetUserIdAsync(_user);
+        _recoveryCodes = await UserManager.GenerateNewTwoFactorRecoveryCodesAsync(_user, 10);
+        _message = "You have generated new recovery codes.";
+
+        Logger.LogInformation("User with ID '{UserId}' generated new 2FA recovery codes.", userId);
+    }
+}
