691cb463c59a63d548db950d88e7ffdd65ac7801
Simon Da Vall <simon.davall@gmail.com>
2026-07-16 12:25:56 +0100

    sot-20: Set url ports and replaced sample endpoint

--- Soteria.ReferenceApi/Program.cs
+++ Soteria.ReferenceApi/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var referenceApiBaseUrl = builder.Configuration["ReferenceApi:BaseUrl"]
+                          ?? throw new InvalidOperationException(
+                              "The ReferenceApi:BaseUrl configuration value is required.");
+
+builder.Services.AddHttpClient("ReferenceApi", client =>
+{
+    client.BaseAddress = new Uri(referenceApiBaseUrl);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,28 +23,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/api/reference", () =>
+        Results.Ok(new
+        {
+            Message = "Reference API reachable"
+        }))
+    .WithName("GetReference");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
