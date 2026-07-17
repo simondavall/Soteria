a2691f7e2177e4153fce86bbc0f50a751f1b928a
Simon Da Vall <simon.davall@gmail.com>
2026-07-17 15:49:44 +0100

    sot-29: Registered the reference web application client.

--- Soteria/Data/OpenIddictInitializer.cs
+++ Soteria/Data/OpenIddictInitializer.cs
@@ -1,16 +1,22 @@
 ﻿using OpenIddict.Abstractions;
+using Permissions = OpenIddict.Abstractions.OpenIddictConstants.Permissions;
 
 namespace Soteria.Data;
 
-public sealed class OpenIddictInitializer(IOpenIddictScopeManager scopeManager)
+public sealed class OpenIddictInitializer(IOpenIddictScopeManager scopeManager, IOpenIddictApplicationManager applicationManager)
 {
     private const string ReferenceApiScope = "reference_api";
     private const string ReferenceApiResource = "reference_api";
+    
+    private const string ReferenceWebClientId = "reference_web";
+    private const string ReferenceWebDisplayName = "Reference Web";
+    private const string ReferenceWebRedirectUri = "https://localhost:7276/signin-oidc";
 
     public async Task InitializeAsync(
         CancellationToken cancellationToken = default)
     {
         await EnsureReferenceApiScopeAsync(cancellationToken);
+        await EnsureReferenceWebApplicationAsync(cancellationToken);
     }
 
     private async Task EnsureReferenceApiScopeAsync(
@@ -31,5 +37,47 @@ public sealed class OpenIddictInitializer(IOpenIddictScopeManager scopeManager)
         descriptor.Resources.Add(ReferenceApiResource);
 
         await scopeManager.CreateAsync(descriptor, cancellationToken);
+    }
+    
+    private async Task EnsureReferenceWebApplicationAsync(
+        CancellationToken cancellationToken)
+    {
+        var application =
+            await applicationManager.FindByClientIdAsync(
+                ReferenceWebClientId,
+                cancellationToken);
+
+        if (application is not null)
+        {
+            return;
+        }
+
+        var descriptor = new OpenIddictApplicationDescriptor
+        {
+            ClientId = ReferenceWebClientId,
+            DisplayName = ReferenceWebDisplayName,
+            ConsentType = OpenIddictConstants.ConsentTypes.Implicit,
+            ClientType = OpenIddictConstants.ClientTypes.Public
+        };
+
+        descriptor.RedirectUris.Add(new Uri(ReferenceWebRedirectUri));
+
+        descriptor.Permissions.UnionWith(
+        [
+            Permissions.Endpoints.Authorization,
+            Permissions.Endpoints.Token,
+
+            Permissions.GrantTypes.AuthorizationCode,
+
+            Permissions.ResponseTypes.Code,
+
+            Permissions.Prefixes.Scope + OpenIddictConstants.Scopes.OpenId,
+            Permissions.Prefixes.Scope + OpenIddictConstants.Scopes.Profile,
+            Permissions.Prefixes.Scope + ReferenceApiScope
+        ]);
+
+        descriptor.Requirements.Add(OpenIddictConstants.Requirements.Features.ProofKeyForCodeExchange);
+
+        await applicationManager.CreateAsync(descriptor, cancellationToken);
     }
 }
