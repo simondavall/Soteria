6d07e8f623bb6975149b9eaa4d1f09454995a4d9
Simon Da Vall <simon.davall@gmail.com>
2026-07-24 18:02:01 +0100

    sot-52: Implemented User Details

--- Soteria/Components/Features/Users/Pages/UserDetails.razor.cs
+++ Soteria/Components/Features/Users/Pages/UserDetails.razor.cs
@@ -5,10 +5,34 @@ namespace Soteria.Components.Features.Users.Pages;
 public partial class UserDetails
 {
     [Inject]
+    private UserService UserService { get; set; } = default!;
+    [Inject]
     private NavigationManager NavigationManager { get; set; } = default!;
 
     [Parameter]
     public Guid UserId { get; set; }
+
+    private UserDetailsModel? User { get; set; }
+    private bool IsLoading { get; set; }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        IsLoading = true;
+
+        try
+        {
+            User = await UserService.GetUserAsync(UserId);
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    private static string FormatLockoutEnd(DateTimeOffset? lockoutEnd)
+    {
+        return lockoutEnd?.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss 'UTC'") ?? string.Empty;
+    }
 
     private void ReturnToUserList()
     {
