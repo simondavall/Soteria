da488cf5532fb8193a010f559c4b3a06c8bdb641
Simon Da Vall <simon.davall@gmail.com>
2026-07-30 19:32:31 +0100

    sot-86: Configured persistent ASP.NET Core Data Protection for Production

---
+++ Soteria/Data/DataProtectionServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace Soteria.Data;
+
+internal static class DataProtectionServiceCollectionExtensions
+{
+    private const string ApplicationName = "Soteria";
+    private const string ProductionKeyRingPath =
+        @"C:\ProgramData\Soteria\DataProtection";
+
+    public static IServiceCollection AddSoteriaDataProtection(this IServiceCollection services, IWebHostEnvironment environment)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(environment);
+
+        if (environment.IsDevelopment())
+        {
+            return services;
+        }
+
+        Directory.CreateDirectory(ProductionKeyRingPath);
+
+        services
+            .AddDataProtection()
+            .SetApplicationName(ApplicationName)
+            .PersistKeysToFileSystem(new DirectoryInfo(ProductionKeyRingPath))
+            .ProtectKeysWithDpapi(protectToLocalMachine: true);
+
+        return services;
+    }
+}
